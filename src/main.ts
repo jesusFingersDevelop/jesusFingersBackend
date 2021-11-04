75868b609325cddf3eae2d3825fe5da88f2285cf
11t518s <11t518s20161759@gmail.com>
2021-11-04 19:47:58 +0900

    chore: api docs security api 문서 접근 시 확인을 요함 #6

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import expressBasicAuth from 'express-basic-auth';
 import { AppModule } from './app.module';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT;
 
+  app.use(
+    ['/api', '/api-json'],
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
+      },
+    }),
+  );
   const config = new DocumentBuilder()
     .setTitle('Jesusfingers')
     .setDescription('Jesusfingers API description')
     .setVersion('1.0')
-    .addTag('hi developers')
+    .addTag('열심히 만들어보자구 ㅎ_ㅎ')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  const PORT = process.env.PORT;
   await app.listen(PORT);
-
   console.log(`hello this is ${PORT} port`);
   if (module.hot) {
     module.hot.accept();
