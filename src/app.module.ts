e736d33a84b027ce3ba4b6f1c1f92f34fb47c9cb
11t518s <11t518s20161759@gmail.com>
2021-11-04 23:33:04 +0900

    feature: user controller 유지 db활용을 위한 디렉토리 구조 설정 #12

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,23 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer): any {
+  configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes('*');
   }
 }
---
+++ src/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('api/v1/users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getCurrentUser() {}
+
+  @Post('jwt')
+  async login() {}
+}
