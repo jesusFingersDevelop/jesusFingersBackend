64f444594091151c698bf071ad1667626144e29c
11t518s <11t518s20161759@gmail.com>
2021-11-07 11:46:09 +0900

    changes

--- dist/main.js
+++ dist/main.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const common_1 = require("@nestjs/common");
 const core_1 = require("@nestjs/core");
+const swagger_1 = require("@nestjs/swagger");
 const express_basic_auth_1 = __importDefault(require("express-basic-auth"));
 const app_module_1 = require("./app.module");
 const http_exception_filter_1 = require("./common/exceptions/http-exception.filter");
@@ -18,6 +19,14 @@ async function bootstrap() {
             [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
         },
     }));
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('Jesusfingers')
+        .setDescription('Jesusfingers API description')
+        .setVersion('1.0')
+        .addTag('열심히 만들어보자구 ㅎ_ㅎ')
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api', app, document);
     app.enableCors({
         origin: true,
         credentials: true,
